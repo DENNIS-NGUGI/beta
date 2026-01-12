c1f6a0b019f23eeaa40e4a69fca3628b3130235d
Moh <official.gitari@gmail.com>
2026-01-12 12:14:40 +0300

    chnaged url for admin

--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('db2duKpzJrZUaUxlODHnXpMEEJ_5cs7rtK4gb9L_0LI/', admin.site.urls),
     path('', include('betadash.urls')),
     path('beta/auth/', include('beta_auth.urls'))
 ]
